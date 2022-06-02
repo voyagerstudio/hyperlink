579710f210b435592b0056d9f936105ed8697605
BrendanMontalvo <96041850+BrendanMontalvo@users.noreply.github.com>
2022-06-02 18:26:35 -0500

    figuring out the disable state prop, and ARIA.

--- components/util-button/UtilButton.js
+++ components/util-button/UtilButton.js
@@ -5,13 +5,14 @@ import styles from './UtilButton.module.css'
 
 
 export function UtilButton({label, style, size}) {
+  const [disable, setDisable] = React.useState(false);
   return (
-    <button type="button" className={`${styles.btn} btn--${style} ${styles.btn}--${size} }`}>{label}</button>
+    <button disabled={disable} onClick={() => setDisable(true)} type="button" className={`${styles.btn} btn--${style} ${styles.btn}--${size}`}>{label}</button>
   )
 }
 
 UtilButton.propTypes = {
   label: PropTypes.string.isRequired,
   style: PropTypes.oneOf(['primary', 'secondary']),
-  size: PropTypes.oneOf(['small', 'medium', 'large'])
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
 }
